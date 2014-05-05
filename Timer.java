cebe212da38223fc733f6889d7050dc5a600b148
joxar <mfpirlo@yahoo.co.jp>
2014-05-06 02:08:43 +0900

    create Timer.java

---
+++ Timer.java
@@ -0,0 +1,44 @@
+package tool;
+
+/*
+ * This class is for measuring the running time of program
+ */
+public class Timer implements Tool {
+	private String procName = "";
+	private long start;
+	private long stop;
+
+	/*
+	 * Constructor
+	 * @param String name
+	 */
+	public Timer(String name) {
+		this.procName = name;
+	}
+	
+	/*
+	 * @param
+	 * @return
+	 */
+	public void startTimer() {
+		start = System.currentTimeMillis();
+	}
+	
+	/*
+	 * @param
+	 * @return
+	 */
+	public void stopTimer() {
+	    stop = System.currentTimeMillis();
+	}
+	
+	/*
+	 * @param
+	 * @return
+	 */
+	public void dispResult() {
+	    System.out.println("ProcName: " + procName);
+	    System.out.println("time: " + (stop - start) + "  mili seconds.");		
+	}
+	
+}
