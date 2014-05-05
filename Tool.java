c4b5daae3b4496b6fda81b7d3a7605e36279616c
joxar <mfpirlo@yahoo.co.jp>
2014-05-06 02:20:46 +0900

    create Tool.java

---
+++ Tool.java
@@ -0,0 +1,11 @@
+package tool;
+
+/*
+ * This class is an interface for common tools
+ */ 
+public interface Tool {
+    public abstract void startTimer();
+    public abstract void stopTimer();
+    public abstract void dispResult();
+    public abstract void anagramChecker();
+}
